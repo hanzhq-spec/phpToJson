cc91b2e1b103192d913bd6dd0ba414254c1703cc
hanzhq-macAir <hanzhq-macAir>
2022-03-16 19:47:14 +0800

    master

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
  * outPutJsonBookData v1.0 (https://github.com/hanzhq-spec/outputjson)
  * Copyright hanzhq-spec
  * Licensed under MIT
- */
+ *
+ * Purpose
+ * 输出指定格式BookData.json文件到当前目录，并提供下载。
+ *
 /*!*/
 function strsToArray($bookName,$bookId) { 
   $bookCover = '/Public/editor/attached/image/20210326/';
@@ -61,21 +64,19 @@ if(isset($_POST["bookName"])&&$_POST["bookId"]){
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>生成JSON文件小程序 - 信息中心</title>
+  <title>输出Json文件工具</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
 <div class="form-signin">
-  <h2>生成JSON文件小程序 - ver1.0</h2>  
+  <h2>输出Json文件工具 - ver1.1</h2>  
   <form action="" method="post">
   <textarea placeholder="一行一本书" required name="bookName"   cols="40"   rows="24"></textarea>
   <textarea placeholder="对应每行书的Cover" required name="bookId"   cols="40"   rows="24"></textarea>
   <br>
   <input style="width:40%" type="submit" value="生成JSON"> <span <?php echo $display ?>><a href="BookData.json" download="w3logo">点击下载</a></span>
   </form>  
-
-
 </div>
 
 </body>
